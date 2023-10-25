0a4e47bd236fa7534bbfa42bfaa9832113b8d308
a-ni-sha <76418971+a-ni-sha@users.noreply.github.com>
2023-10-24 22:41:02 -0400

    Add files via upload

--- TurkishLinguisticsApp.java
+++ TurkishLinguisticsApp.java
@@ -6,13 +6,12 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
 public class TurkishLinguisticsApp {
     private static Map<String, String> turkishWords = new HashMap<>();
-    private static JPanel mainPanel;
-    private static JPanel learnPanel;
-    private static JPanel testPanel;
-    private static CardLayout cardLayout;
+    private static int score = 0;
+    private static int currentWordIndex = 0;
 
     public static void main(String[] args) {
         turkishWords.put("Merhaba", "Hello");
@@ -21,72 +20,78 @@ public class TurkishLinguisticsApp {
         turkishWords.put("Hayır", "No");
         turkishWords.put("Lütfen", "Please");
 
-        JFrame frame = new JFrame("Basic Turkish Linguistics App");
+        JFrame frame = new JFrame("Advanced Turkish Linguistics App");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(400, 300);
 
-        mainPanel = new JPanel();
-        learnPanel = new JPanel();
-        testPanel = new JPanel();
-        cardLayout = new CardLayout();
-        mainPanel.setLayout(cardLayout);
+        JTabbedPane tabbedPane = new JTabbedPane();
+        JPanel learnPanel = createLearnPanel();
+        JPanel testPanel = createTestPanel();
+        tabbedPane.addTab("Learn Turkish", learnPanel);
+        tabbedPane.addTab("Test Your Knowledge", testPanel);
 
-        createLearnPanel();
-        createTestPanel();
-
-        mainPanel.add(learnPanel, "Learn");
-        mainPanel.add(testPanel, "Test");
-
-        JButton learnButton = new JButton("Learn Turkish");
-        learnButton.addActionListener(e -> cardLayout.show(mainPanel, "Learn"));
-
-        JButton testButton = new JButton("Test Your Knowledge");
-        testButton.addActionListener(e -> cardLayout.show(mainPanel, "Test"));
-
-        JPanel buttonPanel = new JPanel();
-        buttonPanel.add(learnButton);
-        buttonPanel.add(testButton);
-
-        frame.add(mainPanel, BorderLayout.CENTER);
-        frame.add(buttonPanel, BorderLayout.SOUTH);
+        frame.add(tabbedPane);
 
         frame.setVisible(true);
     }
 
-    private static void createLearnPanel() {
-        learnPanel.setLayout(new GridLayout(0, 2));
+    private static JPanel createLearnPanel() {
+        JPanel panel = new JPanel(new GridLayout(0, 2));
         for (Map.Entry<String, String> entry : turkishWords.entrySet()) {
             JLabel turkishLabel = new JLabel(entry.getKey());
             JLabel englishLabel = new JLabel(entry.getValue());
-            learnPanel.add(turkishLabel);
-            learnPanel.add(englishLabel);
+            panel.add(turkishLabel);
+            panel.add(englishLabel);
         }
+        return panel;
     }
 
-    private static void createTestPanel() {
-        testPanel.setLayout(new GridLayout(0, 3));
-        for (Map.Entry<String, String> entry : turkishWords.entrySet()) {
-            JLabel turkishLabel = new JLabel(entry.getKey());
-            JTextField translationField = new JTextField(10);
-            JButton checkButton = new JButton("Check");
+    private static JPanel createTestPanel() {
+        JPanel panel = new JPanel(new GridLayout(0, 2));
 
-            checkButton.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    String userTranslation = translationField.getText();
-                    if (userTranslation.equalsIgnoreCase(entry.getValue())) {
-                        JOptionPane.showMessageDialog(testPanel, "Correct!");
-                    } else {
-                        JOptionPane.showMessageDialog(testPanel, "Incorrect. The correct answer is '" + entry.getValue() + "'.");
-                    }
+        JLabel wordLabel = new JLabel("Translate: ");
+        JLabel currentWord = new JLabel(getRandomWord());
+
+        JTextField translationField = new JTextField(15);
+        JButton checkButton = new JButton("Check");
+
+        JLabel scoreLabel = new JLabel("Score: " + score); // New score label
+
+        checkButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String userTranslation = translationField.getText();
+                String correctTranslation = turkishWords.get(currentWord.getText());
+
+                if (userTranslation.equalsIgnoreCase(correctTranslation)) {
+                    JOptionPane.showMessageDialog(panel, "Correct!");
+                    score++;
+                } else {
+                    JOptionPane.showMessageDialog(panel, "Incorrect. The correct answer is '" + correctTranslation + "'.");
                 }
-            });
 
-            testPanel.add(turkishLabel);
-            testPanel.add(translationField);
-            testPanel.add(checkButton);
-        }
+                currentWord.setText(getRandomWord());
+                translationField.setText("");
+                scoreLabel.setText("Score: " + score); // Update the score label
+            }
+        });
+
+        panel.add(wordLabel);
+        panel.add(currentWord);
+        panel.add(translationField);
+        panel.add(checkButton);
+        panel.add(scoreLabel); // Add the score label to the panel
+
+        return panel;
+    
+    }
+
+    private static String getRandomWord() {
+        String[] words = turkishWords.keySet().toArray(new String[0]);
+        Random random = new Random();
+        int randomIndex = random.nextInt(words.length);
+        currentWordIndex = randomIndex;
+        return words[currentWordIndex];
     }
 }
-
 
